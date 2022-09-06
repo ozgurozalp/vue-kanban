b609b35242249b7e195350c6d977e4a404296270
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-09-07 02:36:35 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,vue}'],
+	theme: {
+		extend: {},
+		fontFamily: {
+			serif: ['"IBM Plex Sans"', ...defaultTheme.fontFamily.serif],
+		},
+	},
+	plugins: [],
+};
