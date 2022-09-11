50da8049a3873430c894a04f9eb1c60d21f2859f
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-09-11 18:01:16 +0300

    bazı paketler yüklendi

---
+++ src/store/index.ts
@@ -0,0 +1,44 @@
+import { ref } from 'vue';
+import { defineStore } from 'pinia';
+import { data } from '@/constants/data';
+import { useLocalStorage } from '@vueuse/core';
+import type { Task } from '@/types';
+
+export const useStore = defineStore('kanbanStore', () => {
+	const sections = ref(useLocalStorage('sections', data));
+
+	function addSection() {
+		sections.value.push({
+			id: crypto.randomUUID(),
+			sectionName: 'New Section ' + (sections.value.length + 1),
+			tasks: [],
+			order: sections.value.length + 1,
+		});
+	}
+
+	function removeSection(id: string) {
+		sections.value = sections.value.filter(section => section.id !== id);
+	}
+
+	function updateSectionName(id: string, name: string) {
+		const section = sections.value.find(section => section.id === id);
+		if (section) {
+			section.sectionName = name;
+		}
+	}
+
+	function toggleFavorite(id: string) {
+		const task = findTask(id);
+		if (task) {
+			task.isFavorite = !task.isFavorite;
+			task.favoriteCount += task.isFavorite ? 1 : -1;
+		}
+	}
+
+	function findTask(id: string): Task | undefined {
+		// @ts-ignore
+		return sections.value.flatMap(section => section.tasks).find(task => task.id === id);
+	}
+
+	return { sections, addSection, removeSection, updateSectionName, toggleFavorite };
+});
