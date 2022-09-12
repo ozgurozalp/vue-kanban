583924a039f06064a4d22192010936f9f2e44bf2
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-09-13 01:49:41 +0300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,10 @@ import { createPinia } from 'pinia';
 import App from './App.vue';
 import './assets/app.css';
 import { router } from '@/router';
+import Popper from 'vue3-popper';
 
 const app = createApp(App);
 app.use(router);
 app.use(createPinia());
+app.component('Popper', Popper);
 app.mount('#app');
