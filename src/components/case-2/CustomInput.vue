2a7ab7737d6cba06b9371454b2f86616b054b9cf
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-09-13 01:50:26 +0300

    Create CustomInput.vue

---
+++ src/components/case-2/CustomInput.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Info from '@/components/case-2/Info.vue';
+defineProps<{
+	description?: string;
+	label?: string;
+	modelValue: string | boolean | number;
+	name?: string;
+}>();
+</script>
+
+<template>
+	<label class="flex flex-col gap-2">
+		<span class="flex items-center gap-2" v-if="label">
+			{{ label }}
+			<Info v-if="description" :text="description" />
+		</span>
+		<input type="text" :name="name" @input="$emit('update:modelValue', $event.target.value)" :value="modelValue" />
+	</label>
+</template>
