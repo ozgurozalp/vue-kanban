b4e1b2a00ba912a526743bec30707cb712b07276
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-09-14 02:37:50 +0300

    Create EditPost.vue

---
+++ src/pages/EditPost.vue
@@ -0,0 +1,23 @@
+<template>
+	<FormBuilder :options="options" @submit="addPost" />
+</template>
+<script setup lang="ts">
+import { ref } from 'vue';
+
+import { customFields } from '@/constants';
+import FormBuilder from '@/components/case-2/FormBuilder.vue';
+import { usePostStore } from '@/store';
+import { useRouter } from 'vue-router';
+
+const options = ref(customFields);
+const store = usePostStore();
+const router = useRouter();
+
+function addPost(data: any) {
+	const post = {
+		[crypto.randomUUID()]: data,
+	};
+	store.addPost(post);
+	router.push({ name: 'case-2' });
+}
+</script>
