b39daf47c1033491dcdf23f463adffb8d8e5913f
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-09-14 02:38:00 +0300

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,11 +2,27 @@ import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/pages/Home.vue';
 import Case1 from '@/pages/Case1.vue';
 import Case2 from '@/pages/Case2.vue';
+import AddPost from '@/pages/AddPost.vue';
+import EditPost from '@/pages/EditPost.vue';
 
 const routes = [
-	{ path: '/', component: Home },
+	{ path: '/', name: 'case-1', component: Home },
 	{ path: '/case-1', component: Case1 },
-	{ path: '/case-2', component: Case2 },
+	{
+		path: '/case-2',
+		component: Case2,
+		name: 'case-2',
+	},
+	{
+		path: '/case-2/add-post',
+		component: AddPost,
+		name: 'add-post',
+	},
+	{
+		path: '/case-2/post/:id',
+		component: EditPost,
+		name: 'edit-post',
+	},
 ];
 
 export const router = createRouter({
