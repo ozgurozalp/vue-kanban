afc2476a5b13e254424d9990140ca1ec2a808a58
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-09-14 02:38:53 +0300

    Update CustomInput.vue

--- src/components/case-2/CustomInput.vue
+++ src/components/case-2/CustomInput.vue
@@ -1,19 +1,32 @@
 <script setup lang="ts">
 import Info from '@/components/case-2/Info.vue';
+
 defineProps<{
 	description?: string;
 	label?: string;
 	modelValue: string | boolean | number;
 	name?: string;
+	placeholder?: string;
+	validator?: any;
 }>();
 </script>
 
 <template>
 	<label class="flex flex-col gap-2">
-		<span class="flex items-center gap-2" v-if="label">
+		<span class="flex items-center gap-2 text-sm sm:text-xl" v-if="label">
 			{{ label }}
 			<Info v-if="description" :text="description" />
 		</span>
-		<input type="text" :name="name" @input="$emit('update:modelValue', $event.target.value)" :value="modelValue" />
+		<input
+			:placeholder="placeholder"
+			type="text"
+			:name="name"
+			@input="$emit('update:modelValue', $event.target.value)"
+			:value="modelValue"
+			@blur="validator.$touch()"
+		/>
+		<span class="text-sm text-red-500" v-if="validator.$dirty && validator.$invalid">
+			{{ validator.require.$message }}
+		</span>
 	</label>
 </template>
