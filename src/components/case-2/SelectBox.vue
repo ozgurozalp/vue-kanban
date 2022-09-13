3c3b963f2cef370751633e82cb59a7e8f88744d4
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-09-14 02:38:44 +0300

    Update SelectBox.vue

--- src/components/case-2/SelectBox.vue
+++ src/components/case-2/SelectBox.vue
@@ -16,6 +16,7 @@ const props = defineProps<{
 		value: string;
 	};
 	name?: string;
+	validator?: any;
 }>();
 
 const emit = defineEmits(['update:modelValue']);
@@ -29,17 +30,22 @@ function changeHandler(event: InputEvent) {
 
 <template>
 	<label class="flex flex-col gap-2">
-		<span class="flex items-center gap-2" v-if="label">
+		<span class="flex items-center gap-2 text-sm sm:text-xl" v-if="label">
 			{{ label }}
 			<Info v-if="description" :text="description" />
 		</span>
 		<span class="relative">
-			<select :name="name" @change="changeHandler" :value="modelValue">
+			<select :name="name" @change="changeHandler" :value="modelValue" @blur="validator.$touch()">
 				<option :value="option.id" v-for="option in data" :key="option.id">
 					{{ option.name }}
 				</option>
 			</select>
-			<span v-if="modelValue?.value" class="absolute left-3 top-1/2 -translate-y-1/2">{{ modelValue.name }}</span>
+			<span v-if="modelValue?.value" class="absolute left-3 top-1/2 z-10 -translate-y-1/2 bg-white">
+				{{ modelValue.name }}
+			</span>
+		</span>
+		<span class="text-sm text-red-500" v-if="validator.$dirty && validator.$invalid">
+			{{ validator.require.$message }}
 		</span>
 	</label>
 </template>
