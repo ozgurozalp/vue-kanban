a0667d7e0a6e073924d1101589f309393a4b9193
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-09-14 18:56:56 +0300

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,3 +3,15 @@
 		<RouterView />
 	</div>
 </template>
+
+<script setup lang="ts">
+import { usePostStore } from '@/store';
+import { onMounted } from 'vue';
+
+const store = usePostStore();
+
+onMounted(() => {
+	store.fetchFields();
+	store.fetchPosts();
+});
+</script>
