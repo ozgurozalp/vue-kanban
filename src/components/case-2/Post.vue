ed3e269288a174f0899ffa4cde9d91514685b421
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-09-14 19:01:36 +0300

    Update Post.vue

--- src/components/case-2/Post.vue
+++ src/components/case-2/Post.vue
@@ -1,9 +1,9 @@
 <template>
 	<article class="relative flex flex-col gap-2 bg-white p-4 shadow">
 		<h1 class="text-2xl font-bold">{{ parsedPost.Title }}</h1>
-		<p class="text-sm">{{ parsedPost.Category.name }}</p>
+		<p class="text-sm">{{ parsedPost?.Category?.name }}</p>
 		<p class="text-sm">{{ new Date(parsedPost['Created Date']).toDateString() }}</p>
-		<p class="text-md flex-1 text-justify">{{ parsedPost.Details }}</p>
+		<p class="text-md flex-1 text-justify">{{ parsedPost?.Details }}</p>
 		<p class="text-right text-orange-500">
 			<span v-if="!parsedPost['Allow Comments']">not</span> open for comments
 		</p>
@@ -19,8 +19,7 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref } from 'vue';
-import { customFields } from '@/constants';
+import { computed } from 'vue';
 import IconTrash from '@/components/icons/IconTrash.vue';
 import IconPencil from '@/components/icons/IconPencil.vue';
 import { usePostStore } from '@/store';
@@ -28,19 +27,19 @@ const store = usePostStore();
 
 const props = defineProps<{
 	post: object;
+	postId: string;
 }>();
 
-const postId = ref(Object.keys(props.post)[0]);
-
 function deletePost() {
-	store.removePost(postId.value);
+	store.removePost(props.postId);
 }
+
 const parsedPost = computed(() => {
-	return customFields.reduce((acc, curr) => {
+	return store.fields.reduce((acc, curr) => {
 		return {
 			...acc,
 			// @ts-ignore
-			[curr.name]: props.post[postId.value][`cf_${curr.id}`],
+			[curr.name]: props.post[`cf_${curr.id}`],
 		};
 	}, {});
 });
