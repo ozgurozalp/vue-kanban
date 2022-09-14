00c4a98685efeec95d688f2298ca6f38e6655ef9
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-09-14 18:55:38 +0300

    Update AddPost.vue

--- src/pages/AddPost.vue
+++ src/pages/AddPost.vue
@@ -1,23 +1,19 @@
 <template>
-	<FormBuilder :options="options" @submit="addPost" />
+	<FormBuilder :fields="fields" @submit="addPost" />
 </template>
 <script setup lang="ts">
-import { ref } from 'vue';
-
-import { customFields } from '@/constants';
 import FormBuilder from '@/components/case-2/FormBuilder.vue';
 import { usePostStore } from '@/store';
 import { useRouter } from 'vue-router';
+import { computed } from 'vue';
 
-const options = ref(customFields);
 const store = usePostStore();
 const router = useRouter();
+const fields = computed(() => store.fields);
 
 function addPost(data: any) {
-	const post = {
-		[crypto.randomUUID()]: data,
-	};
-	store.addPost(post);
+	const id = crypto.randomUUID();
+	store.addPost(id, data);
 	router.push({ name: 'case-2' });
 }
 </script>
