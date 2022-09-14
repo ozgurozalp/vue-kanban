156e35a5601de816072f887cd173586555b1e8bf
Özgür ÖZALP <21113261+ozgurozalp@users.noreply.github.com>
2022-09-14 18:43:15 +0300

    Update Home.vue

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,6 @@
 <template>
 	<div class="flex h-full flex-col place-items-center justify-center">
-		<h1 class="mb-5 text-8xl">Welcome To Project</h1>
+		<h1 class="mb-5 text-center text-8xl">Welcome To Project</h1>
 		<nav class="flex gap-10 [&>a]:text-5xl">
 			<RouterLink :to="{ name: 'case-1' }" class="link">Case 1</RouterLink>
 			<RouterLink :to="{ name: 'case-2' }" class="link">Case 2</RouterLink>
